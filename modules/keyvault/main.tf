f5645b8924ac3312ecada4f82a5c59d8989d780d
ugochuk <96281887+ugochuk@users.noreply.github.com>
2026-08-16 16:36:22 -0700

    Add secure Key Vault with private endpoint

---
+++ modules/keyvault/main.tf
@@ -0,0 +1,49 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "this" {
+  name                       = substr("kv-${var.prefix}-${var.environment}-platform", 0, 24)
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  enable_rbac_authorization  = true
+  purge_protection_enabled   = true
+  soft_delete_retention_days = 7
+  public_network_access_enabled = false
+  tags                       = var.tags
+}
+
+resource "azurerm_private_dns_zone" "keyvault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
+  name                  = "link-${var.prefix}-${var.environment}-kv"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.keyvault.name
+  virtual_network_id    = var.spoke_vnet_id
+  registration_enabled  = false
+  tags                  = var.tags
+}
+
+resource "azurerm_private_endpoint" "keyvault" {
+  name                = "pep-${var.prefix}-${var.environment}-kv"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+  tags                = var.tags
+
+  private_service_connection {
+    name                           = "psc-${var.prefix}-${var.environment}-kv"
+    private_connection_resource_id = azurerm_key_vault.this.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.keyvault.id]
+  }
+}
