bb409cf82e4795029f6211febb412c6064cbfb00
ugochuk <96281887+ugochuk@users.noreply.github.com>
2026-08-16 16:33:53 -0700

    Add monitoring module variables

---
+++ modules/monitoring/variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "prefix" { type = string }
+variable "environment" { type = string }
+variable "tags" { type = map(string) }
