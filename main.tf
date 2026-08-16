6fe3b4aa36270527ccc3c69a604c2f31015c2f9d
ugochuk <96281887+ugochuk@users.noreply.github.com>
2026-08-16 16:33:24 -0700

    Add landing zone composition

---
+++ main.tf
@@ -0,0 +1,46 @@
+locals {
+  common_tags = merge(
+    {
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Project     = "AzureEnterpriseLandingZone"
+    },
+    var.tags
+  )
+}
+
+resource "azurerm_resource_group" "network" {
+  name     = "rg-${var.prefix}-${var.environment}-network"
+  location = var.location
+  tags     = local.common_tags
+}
+
+resource "azurerm_resource_group" "management" {
+  name     = "rg-${var.prefix}-${var.environment}-management"
+  location = var.location
+  tags     = local.common_tags
+}
+
+module "network" {
+  source = "./modules/network"
+
+  resource_group_name   = azurerm_resource_group.network.name
+  location              = var.location
+  prefix                = var.prefix
+  environment           = var.environment
+  hub_address_space     = var.hub_address_space
+  spoke_address_space   = var.spoke_address_space
+  hub_subnet_prefixes   = var.hub_subnet_prefixes
+  spoke_subnet_prefixes = var.spoke_subnet_prefixes
+  tags                  = local.common_tags
+}
+
+module "monitoring" {
+  source = "./modules/monitoring"
+
+  resource_group_name = azurerm_resource_group.management.name
+  location            = var.location
+  prefix              = var.prefix
+  environment         = var.environment
+  tags                = local.common_tags
+}
